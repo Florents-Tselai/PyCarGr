762dc1c48b6f903241f9a0d6a86eb47efcf0ee8c
Florents Tselai <florents.tselai@gmail.com>
2018-10-22 16:29:38 +0300

    Add seed script

--- seed.py
+++ seed.py
@@ -1,12 +1,29 @@
-from pycargr.parser import parse_search_results
-from pycargr import save_car
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from tqdm import tqdm
+
+from pycargr import save_car
+from pycargr.parser import parse_search_results
+
 SEARCH_URL = "https://www.car.gr/classifieds/cars/?fs=1&condition=%CE%9A%CE%B1%CE%B9%CE%BD%CE%BF%CF%8D%CF%81%CE%B9%CE%BF&condition=%CE%9C%CE%B5%CF%84%CE%B1%CF%87%CE%B5%CE%B9%CF%81%CE%B9%CF%83%CE%BC%CE%AD%CE%BD%CE%BF&offer_type=sale"
+MAX_PAGES = 10 ** 4
+MAX_WORKERS = 10
+
+
+def work_page_id(page_url):
+    for c in parse_search_results(page_url):
+        save_car(c)
 
 
 def main():
-    for c in tqdm(parse_search_results(SEARCH_URL)):
-        save_car(c)
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        fts = []
+        for page in range(1, MAX_PAGES):
+            fts.append(executor.submit(work_page_id, SEARCH_URL + '&pg=%d' % page))
+
+        for _ in tqdm(as_completed(fts), total=MAX_PAGES, unit='page'):
+            pass
+
 
 if __name__ == '__main__':
     main()
