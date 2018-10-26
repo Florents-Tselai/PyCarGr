301f9912984b1b836f5a40ee6122778451390e74
Florents Tselai <florents.tselai@gmail.com>
2018-10-26 12:40:54 +0300

    Use generator during save

--- pycargr/__init__.py
+++ pycargr/__init__.py
@@ -10,10 +10,9 @@ SEARCH_BASE_URL = 'https://www.car.gr/classifieds/cars/'
 
 
 def save_car(*cars):
-    assert all(isinstance(c, Car) for c in cars)
     with connect(str(DB_PATH), timeout=10) as db:
         db.executemany("INSERT OR REPLACE INTO cars VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                       [(c.car_id, c.title, c.price, c.release_date, c.km, c.bhp, c.url, c.color, c.fueltype,
+                       ((c.car_id, c.title, c.price, c.release_date, c.km, c.bhp, c.url, c.color, c.fueltype,
                          c.description, c.city, c.region, c.postal_code, c.transmission, dumps(c.images), c.html,
                          c.scraped_at) for c in
-                        cars])
+                        cars))
